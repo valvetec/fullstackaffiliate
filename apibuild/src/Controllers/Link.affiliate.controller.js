cbfe46ec6f84869badbda52d4976794f7f4c88c0
unknown <conventionalanimation@gmail.com>
2023-06-17 14:11:14 +0100

    created a build for the affiliate earning

--- apibuild/src/Controllers/Link.affiliate.controller.js
+++ apibuild/src/Controllers/Link.affiliate.controller.js
@@ -19,6 +19,7 @@ const httpCreateLinkAffiliate = (req, res) => __awaiter(void 0, void 0, void 0, 
     const affiliateLink = crypto.randomBytes(16).toString("hex");
     const userId = req.body.userId;
     const productId = req.body.productId;
+    console.log(productId, 'mee');
     try {
         const user = yield AffiliateUsers_mode_1.CreateAffiliateUser.findOne({ where: { id: userId } });
         const product = yield Product_model_1.CreateProduct.findOne({ where: { id: productId } });
@@ -28,11 +29,11 @@ const httpCreateLinkAffiliate = (req, res) => __awaiter(void 0, void 0, void 0, 
         if (!user) {
             return res.status(500).json('no user found');
         }
-        const checkLink = yield Link_affiliate_model_1.CreateLinkeAffiliate.findOne({ where: { affiliateOwnerId: user === null || user === void 0 ? void 0 : user.dataValues.id, productId: product === null || product === void 0 ? void 0 : product.dataValues.id } });
+        const checkLink = yield Link_affiliate_model_1.CreateLinkeAffiliate.findOne({ where: { affiliateOwnerId: user === null || user === void 0 ? void 0 : user.dataValues.id, } });
         if (checkLink) {
             const response = {
                 id: checkLink === null || checkLink === void 0 ? void 0 : checkLink.dataValues.id,
-                productId: checkLink === null || checkLink === void 0 ? void 0 : checkLink.dataValues.productId,
+                productId: product === null || product === void 0 ? void 0 : product.dataValues.id,
                 ownerId: checkLink === null || checkLink === void 0 ? void 0 : checkLink.dataValues.affiliateOwnerId,
                 link: checkLink === null || checkLink === void 0 ? void 0 : checkLink.dataValues.link
             };
@@ -50,6 +51,7 @@ const httpCreateLinkAffiliate = (req, res) => __awaiter(void 0, void 0, void 0, 
             ownerId: createLink === null || createLink === void 0 ? void 0 : createLink.dataValues.affiliateOwnerId,
             link: createLink === null || createLink === void 0 ? void 0 : createLink.dataValues.link
         };
+        yield user.update({ affiliateLink: response === null || response === void 0 ? void 0 : response.link });
         return res.status(200).json(response);
     }
     catch (error) {
